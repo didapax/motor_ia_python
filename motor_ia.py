98da2ce4e18a9113bb5f29d1b3953cae326bc6c2
didapax <alfonsi.acosta@gmail.com>
2024-07-15 11:56:21 -0400

    Update motor_ia.py

--- motor_ia.py
+++ motor_ia.py
@@ -9,6 +9,68 @@ import random
 import time
 
 
+def leer_archivo_json(ruta_archivo):
+    try:
+        with open(ruta_archivo, "r") as archivo:
+            datos_json = json.load(archivo)
+            
+            if isinstance(datos_json, list):
+                return datos_json
+            elif isinstance(datos_json, dict):
+                return list(datos_json)
+            else:
+                print("El archivo JSON contiene otro tipo de datos.")
+
+    except FileNotFoundError:
+        print(f"El archivo '{ruta_archivo}' no existe.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error al decodificar el archivo JSON '{ruta_archivo}'.")
+        return None
+        
+
+def update_clave_json(ruta_archivo, datos):
+    try:
+        with open(ruta_archivo, "w") as archivo:
+            json.dump(datos, archivo, indent=4)
+    except Exception as e:
+        print(f"Error al guardar el archivo JSON: {e}")
+
+
+def update_dic_json(ruta_archivo, datos):
+    try:
+        with open(ruta_archivo, "w") as archivo:
+            json.dump(datos, archivo, indent=4)
+    except Exception as e:
+        print(f"Error al guardar el archivo JSON: {e}")
+
+
+def agregar_datos_json(ruta_archivo, nuevos_datos):
+    datos_json = leer_archivo_json(ruta_archivo)
+    if datos_json is not None:
+        datos_json.update(nuevos_datos)
+        guardar_archivo_json(ruta_archivo, datos_json)
+
+
+def agregar_elemento_lista_json(ruta_archivo, clave_lista="", nuevo_elemento=""):
+    datos_json = leer_archivo_json(ruta_archivo)
+    if datos_json is not None:
+        if clave_lista:
+            if clave_lista in datos_json and isinstance(datos_json[clave_lista], list):
+                datos_json[clave_lista].append(nuevo_elemento)
+            else:
+                print(f"La clave '{clave_lista}' no existe o no es una lista.")
+                return
+        else:
+            if isinstance(datos_json, list):
+                datos_json.append(nuevo_elemento)
+            else:
+                print("El archivo JSON no contiene una lista en la raíz.")
+                return
+
+        update_dic_json(ruta_archivo, datos_json)
+
+
 def insert(res, url="http://localhost:8080/", headers=None):
     if headers is None:
         headers = {"Content-Type": "application/json"}
@@ -19,7 +81,8 @@ def insert(res, url="http://localhost:8080/", headers=None):
     except requests.RequestException as e:
         print(f"Error al enviar la solicitud: {e}")
         return None   
-    
+  
+  
 def obtener_datos_del_backend(text,url="http://localhost:8080/", headers=None):
     if headers is None:
         headers = {"Content-Type": "application/json"}
@@ -68,12 +131,14 @@ def interrogante(text):
 
 def analiza_pregunta(pregunta, tipo, base):
     #base puede ser: "local", "mongo;host_backed_parametro_busqueda_GET",...
-    palabras = pregunta.split()  # Divide la cadena en palabras
-    ultima_palabra = palabras[-1] if palabras else ""  # Obtiene la última palabra
-    result = responder(pregunta, tipo, base)
+    rand = randomBoolean()
+    result = responder(pregunta, rand, base)
     if result == "":
-        result = responder(ultima_palabra, tipo, base)
-    
+        if interrogante(pregunta):
+            palabras = pregunta.split()  # Divide la cadena en palabras
+            ultima_palabra = palabras[-1] if palabras else ""  # Obtiene la última palabra
+            result = responder(ultima_palabra, tipo, base)
+
     return result
 
 
@@ -81,21 +146,22 @@ def responder(text, tipo, base):
     #base puede ser: "local", "mongo;host_backed_parametro_busqueda_GET",...
     mensaje="";
     if base == "local":
-        try:
-            with open(text, "r") as archivo:
-                datos_json = json.load(archivo)
-                mensaje = buscaRespuesta(datos_json, tipo)
-        except FileNotFoundError:
-                #preguntar(text)
-                mensaje="";
-                
+            datos_json = leer_archivo_json('mente.json')            
+            for item in datos_json:
+                if text in item:                    
+                    valor = item[text]
+                    mensaje = buscaRespuesta(valor, tipo)
+
     if "mongo" in base:
         try:
             parametro = base.split(';')
             url_backend = parametro[1]
-            datos_obtenidos = obtener_datos_del_backend(text,url_backend)
-            if datos_obtenidos:            
-                mensaje = buscaRespuesta(datos_obtenidos, tipo)
+            datos_json = obtener_datos_del_backend(text,url_backend)
+            for item in datos_json:
+                if text in item:                    
+                    valor = item[text]
+                    mensaje = buscaRespuesta(valor, tipo)
+                    
         except FileNotFoundError:
                 #preguntar(text)
                 mensaje="";
@@ -103,20 +169,11 @@ def responder(text, tipo, base):
     return mensaje
     
 
-def buscaRespuesta(datos, tipo):
-    """
-    Busca una respuesta o explicación en los datos proporcionados.
-
-    Args:
-        datos (dict): Datos en formato JSON con claves 'explica', 'respuesta' y 'pregunta'.
-        tipo (bool): Indica si se busca una respuesta (True) o una explicación (False).
-
-    Returns:
-        str: Respuesta o explicación combinada con otra pregunta.
-    """
+def buscaRespuesta(datos, tipo):    
     explica = datos.get('explica', '')
     respuesta = datos.get('respuesta', '')
     otra_pregunta = datos.get('otra_pregunta', '')
+    
     resultado = randomBoolean()
 
     if tipo:
@@ -131,11 +188,6 @@ def buscaRespuesta(datos, tipo):
 
 
 def init(texto, base):
-    if interrogante(texto):
-        result = analiza_pregunta(texto, True, base)
-    else:
-        resultado = randomBoolean()
-        result = responder(texto, resultado, base)
-        
-    return result
+    return analiza_pregunta(texto, True, base)
+
             
